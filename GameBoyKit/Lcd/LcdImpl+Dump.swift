5ec5947491a12882e35d032638c03da8d9232dab
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-07-12 19:52:01 +0200

    New debug

--- GameBoyKit/Lcd/LcdImpl+Dump.swift
+++ GameBoyKit/Lcd/LcdImpl+Dump.swift
@@ -5,8 +5,8 @@
 // swiftlint:disable function_body_length
 
 private let tileSize        = 8 // pixels
-private let tileRowCount    = Int(LcdConstants.width)  / tileSize // 18
-private let tileColumnCount = Int(LcdConstants.height) / tileSize // 20
+private let tileRowCount    = 32
+private let tileColumnCount = 32
 private let tilesPerRow = 32
 
 extension LcdImpl {
@@ -15,37 +15,39 @@ extension LcdImpl {
 
   internal func dumpProperties() {
     print("LCDC: \(self.control.bin)")
-    print("isLcdEnabled: \(self.isLcdEnabled)")
-    print("isBackgroundVisible: \(self.isBackgroundVisible)")
-    print("isWindowEnabled: \(self.isWindowEnabled)")
-    print("isSpriteEnabled: \(self.isSpriteEnabled)")
-    print("windowTileMap: \(self.windowTileMap)")
-    print("backgroundTileMap: \(self.backgroundTileMap)")
-    print("tileData: \(self.tileData)")
-    print("spriteHeight: \(self.spriteHeight)")
+    print("  isLcdEnabled: \(self.isLcdEnabled)")
+    print("  isBackgroundVisible: \(self.isBackgroundVisible)")
+    print("  isWindowEnabled: \(self.isWindowEnabled)")
+    print("  isSpriteEnabled: \(self.isSpriteEnabled)")
+    print("  windowTileMap: \(self.windowTileMap)")
+    print("  backgroundTileMap: \(self.backgroundTileMap)")
+    print("  tileDataSelect: \(self.tileDataSelect)")
+    print("  spriteHeight: \(self.spriteHeight)")
 
     print("STAT: \(self.status.bin)")
-    print("isLineCompareInterruptEnabled: \(self.isLineCompareInterruptEnabled)")
-    print("isOamInterruptEnabled: \(self.isOamInterruptEnabled)")
-    print("isVBlankInterruptEnabled: \(self.isVBlankInterruptEnabled)")
-    print("isHBlankInterruptEnabled: \(self.isHBlankInterruptEnabled)")
-    print("isLineCompareInterrupt: \(self.isLineCompareInterrupt)")
-    print("mode: \(self.mode)")
+    print("  isLineCompareInterruptEnabled: \(self.isLineCompareInterruptEnabled)")
+    print("  isOamInterruptEnabled: \(self.isOamInterruptEnabled)")
+    print("  isVBlankInterruptEnabled: \(self.isVBlankInterruptEnabled)")
+    print("  isHBlankInterruptEnabled: \(self.isHBlankInterruptEnabled)")
+    print("  isLineCompareInterrupt: \(self.isLineCompareInterrupt)")
+    print("  mode: \(self.mode)")
 
-    print("scrollY: \(self.scrollY.hex)")
-    print("scrollX: \(self.scrollX.hex)")
-    print("line: \(self.line.hex)")
-    print("lineCompare: \(self.lineCompare.hex)")
-    print("windowY: \(self.windowY.hex)")
-    print("windowX: \(self.windowX.hex)")
-    print("backgroundPalette: \(self.backgroundPalette.hex)")
-    print("spritePalette0: \(self.spritePalette0.hex)")
-    print("spritePalette1: \(self.spritePalette1.hex)")
+    print("ScrollY: \(self.scrollY.hex)")
+    print("ScrollX: \(self.scrollX.hex)")
+    print("Line: \(self.line.hex)")
+    print("LineCompare: \(self.lineCompare.hex)")
+    print("WindowY: \(self.windowY.hex)")
+    print("WindowX: \(self.windowX.hex)")
+    print("BackgroundPalette: \(self.backgroundPalette.hex)")
+    print("SpritePalette0: \(self.spritePalette0.hex)")
+    print("SpritePalette1: \(self.spritePalette1.hex)")
   }
 
   // MARK: - Tile indices
 
   internal func dumpTileIndices(_ map: TileMap) {
+    print("Tile indices \(map):")
+
     // horizontal markers
     print("    | " , separator: "", terminator: "")
     for tileColumn in 0..<tileColumnCount {
@@ -81,52 +83,65 @@ extension LcdImpl {
 
   // MARK: - Tile data
 
-  internal func dumpTileData(_ tileData: TileData) {
-    let range = self.addressRange(tileData)
+  internal func dumpTileData(_ data: TileData) {
+    let columnCount = 16
+    print("Tile data \(data):")
 
-    var address = range.start
-    while address < range.end {
-      let data1 = self.readVideoRam(address)
-      let data2 = self.readVideoRam(address + 1)
-
-      print("\(UInt16(address).hex): \(data1.bin) & \(data2.bin) -> ", separator: "", terminator: "")
-      for i in 0..<8 {
-        let color = self.getColorValue(data1, data2, bit: i)
-        let sColor = color == 0 ? " " : String(describing: color)
-        print(sColor, separator: "", terminator: "")
-      }
-
-      let indexFromStart = (address - range.start) / 2
-      let isTileStart = indexFromStart % 8 == 0
-      if isTileStart {
-        let tileIndex = UInt16(indexFromStart / 8)
-        print(" | Starting tile: \(tileIndex.hex)", separator: "", terminator: "")
-      }
-
-      print()
-
-      address += 2
+    // horizontal markers
+    print("    |" , separator: "", terminator: "")
+    for tileColumn in 0..<columnCount {
+      let text = UInt8(tileColumn).hex
+      print("    \(text)|", separator: "", terminator: "")
     }
-  }
+    print()
 
-  /// Read data from video ram.
-  private func readVideoRam(_ address: Int) -> UInt8 {
-    let start = Int(MemoryMap.videoRam.start)
-    return self.videoRam[address - start]
-  }
+    print("----+" , separator: "", terminator: "")
+    for _ in 0..<columnCount {
+      print("--------+", separator: "", terminator: "")
+    }
+    print()
 
-  private func addressRange(_ data: TileData) -> ClosedRange<Int> {
-    switch data {
-    case .from8800to97ff: return 0x8800...0x97ff
-    case .from8000to8fff: return 0x8000...0x8fff
+    let start = data == .from8000to8fff ?   0 : 128
+    let end   = data == .from8000to8fff ? 256 : 384
+    let tiles = self.tiles[start..<end]
+
+    let rowCount = tiles.count / columnCount
+    for row in 0..<rowCount {
+      for line in 0..<tileSize {
+        if line == 0 {
+          let text = UInt8(row).hex
+          print(text, separator: "", terminator: "|")
+        } else {
+          print("    |", separator: "", terminator: "")
+        }
+
+        for column in 0..<columnCount {
+          let tileIndex = start + row * columnCount + column
+          let tile = tiles[tileIndex]
+
+          for pixel in tile.getPixels(in: line) {
+            let color = pixel == 0 ? " " : String(describing: pixel)
+            print(color, separator: "", terminator: "")
+          }
+          print("|", separator: "", terminator: "")
+        }
+        print()
+      }
+      print("----+" , separator: "", terminator: "")
+      for _ in 0..<columnCount {
+        print("--------+", separator: "", terminator: "")
+      }
+      print()
     }
   }
 
   // MARK: - Background
 
   internal func dumpBackground(_ map: TileMap, _ data: TileData) {
-    let rowRange:    ClosedRange<Int> = 0...tileRowCount
-    let columnRange: ClosedRange<Int> = 0...tileColumnCount
+    print("Background for map: \(map), data: \(data)")
+
+    let rowRange    = 0..<tileRowCount
+    let columnRange = 0..<tileColumnCount
 
     let linesPerTile = 8
 
@@ -135,9 +150,9 @@ extension LcdImpl {
     for tileColumn in columnRange {
       let text = String(describing: tileColumn)
       let padding = String(repeating: " ", count: 8 - text.count)
-      print(padding + text, separator: "", terminator: " ")
+      print(padding + text, separator: "", terminator: "|")
     }
-    print("|")
+    print()
 
     print("------+" , separator: "", terminator: "")
     for _ in columnRange {
@@ -159,7 +174,7 @@ extension LcdImpl {
         print("|", separator: "", terminator: " ")
 
         for tileColumn in columnRange {
-          self.drawTile(map, data, tileRow: tileRow, tileColumn: tileColumn, line: tileLine)
+          self.drawTile(map, data, tileRow, tileColumn, tileLine)
         }
         print()
       }
@@ -172,7 +187,12 @@ extension LcdImpl {
     }
   }
 
-  private func drawTile(_ map: TileMap, _ data: TileData, tileRow: Int, tileColumn: Int, line: Int) {
+  private func drawTile(_ map:  TileMap,
+                        _ data: TileData,
+                        _ tileRow:    Int,
+                        _ tileColumn: Int,
+                        _ tileLine:   Int) {
+
     let tileMap = self.getTileMap(for: map)
     let tileIndexRaw = tileMap[tileRow * tilesPerRow + tileColumn]
 
@@ -181,15 +201,12 @@ extension LcdImpl {
       tileIndex = 256 + Int(Int8(bitPattern: tileIndexRaw))
     }
 
-    let bytesPerLine = 2
-    let tileAddress = (tileIndex * tileSize + line) * bytesPerLine
-    let data1 = self.videoRam[tileAddress]
-    let data2 = self.videoRam[tileAddress + 1]
+    let tile = self.tiles[tileIndex]
+    let tilePixels = tile.getPixels(in: tileLine)
 
-    for i in 0..<8 {
-      let color = self.getColorValue(data1, data2, bit: i)
-      let sColor = color == 0 ? " " : String(describing: color)
-      print(sColor, separator: "", terminator: "")
+    for pixel in tilePixels {
+      let color = pixel == 0 ? " " : String(describing: pixel)
+      print(color, separator: "", terminator: "")
     }
     print("|", separator: "", terminator: "")
   }
