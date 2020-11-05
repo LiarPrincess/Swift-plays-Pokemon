fc0d3cb92fb9ae0671315d8ca12cb4498d128d9d
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-11-05 18:39:53 +0100

    Moved 'Bootrom/Cartridge' memory protocols to Cartridge dir

--- Sources/GameBoyKit/Cartridge/BootromMemory.swift
+++ Sources/GameBoyKit/Cartridge/BootromMemory.swift
@@ -2,19 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import Foundation
-
 internal protocol BootromMemory: AnyObject {
 
   func read(_ address: UInt16) -> UInt8
   func write(_ address: UInt16, value: UInt8)
-}
-
-internal protocol CartridgeMemory: AnyObject {
-
-  func readRom(_ address: UInt16) -> UInt8
-  func writeRom(_ address: UInt16, value: UInt8)
-
-  func readRam(_ address: UInt16) -> UInt8
-  func writeRam(_ address: UInt16, value: UInt8)
 }
---
+++ Sources/GameBoyKit/Cartridge/CartridgeMemory.swift
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+internal protocol CartridgeMemory: AnyObject {
+
+  func readRom(_ address: UInt16) -> UInt8
+  func writeRom(_ address: UInt16, value: UInt8)
+
+  func readRam(_ address: UInt16) -> UInt8
+  func writeRam(_ address: UInt16, value: UInt8)
+}
